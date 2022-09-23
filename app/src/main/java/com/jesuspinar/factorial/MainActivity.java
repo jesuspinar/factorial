117f0eafc229b855bd153d18ad02deb1f84aa528
Jesus <jesuspl@tutanota.com>
2022-09-23 16:28:55 +0200

    Adding factorial method

--- app/src/main/java/com/jesuspinar/factorial/MainActivity.java
+++ app/src/main/java/com/jesuspinar/factorial/MainActivity.java
@@ -12,4 +12,17 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
     }
+
+    public static int factorial(int n) {
+        int result = 1;
+        if(n >= 0){
+            for(int i = 1; i <= n; i++){
+                result *= i;
+            }
+            return result;
+        }
+        else{
+            return result = -1;
+        }
+    }
 }
